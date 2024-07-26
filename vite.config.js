336fb894a900a71253f3a8b973b28ae7974e6126
DevEben <ebenezertope4@gmail.com>
2024-07-26 23:30:46 +0100

    updated the code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure the build output directory is 'dist'
+  },
+  server: {
+    port: 3000, // or any port you prefer
+  },
+});
